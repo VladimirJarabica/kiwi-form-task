ec2ceb860616c9954fb79d66181aba3579a8c1c1
Tibor Gal <gal.tibor.95@gmail.com>
2019-07-10 19:33:02 +0200

    Added missing fields to Passenger

--- src/passenger/Passenger.js
+++ src/passenger/Passenger.js
@@ -1,21 +1,18 @@
 import React from "react";
 import Stack from "@kiwicom/orbit-components/lib/Stack";
-import styled from "styled-components";
 import InputField from "@kiwicom/orbit-components/lib/InputField";
 import InputGroup from "@kiwicom/orbit-components/lib/InputGroup";
 import Select from "@kiwicom/orbit-components/lib/Select";
-
-const Container = styled.div`
-  max-width: 800px;
-  width: 80%;
-  margin: 0 auto;
-`;
 
 function Passenger() {
   return (
     <Stack direction="column">
       <InputField label="First name" size="small" />
       <InputField label="Last name" size="small" />
+      <InputField label="Nationality" size="small" />
+      <InputGroup label="Gender">
+        <Select options={[{value: null, label:'Choose one'},{ value: "male", label: "Male" }, { value: "female", label: "Female" }, { value: "other", label: "Other" }]} value={null}/>
+      </InputGroup>
       <InputGroup
         label="Date of birth"
         flex={["0 0 60px", "1 1 100%", "0 0 90px"]}
@@ -23,9 +20,9 @@ function Passenger() {
         <InputField placeholder="DD" />
         <Select
           options={[
-            { value: "January", label: "January" },
-            { value: "February", label: "February" },
-            { value: "March", label: "March" }
+            { value: "january", label: "January" },
+            { value: "february", label: "February" },
+            { value: "march", label: "March" }
           ]}
           value={null}
           placeholder="Month"
